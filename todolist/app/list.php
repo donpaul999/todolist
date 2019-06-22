671f786bb2c9abda42ca6003e538277297509143
Paul Colța <coltapaulstefan@gmail.com>
2019-06-22 12:16:41 +0300

    log out, fix restriced log in acces from url

--- todolist/app/list.php
+++ todolist/app/list.php
@@ -1,6 +1,8 @@
 <?php
- ?>
-
+session_start();
+if(isset($_SESSION['loggedin']))
+{
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -20,20 +22,28 @@
          <button id="add">
            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"   	 viewBox="0 0 16 16" style="enable-background:new 0 0 16 16;" xml:space="preserve">     <g>     	<g>	<path class="fill" d="M16,8c0,0.5-0.5,1-1,1H9v6c0,0.5-0.5,1-1,1s-1-0.5-1-1V9H1C0.5,9,0,8.5,0,8s0.5-1,1-1h6V1c0-0.5,0.5-1,1-1	s1,0.5,1,1v6h6C15.5,7,16,7.5,16,8z"/></g>   </g>   </svg>
          </button >
-
+       </br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+         <a href = "logout.php"><input type = "button" color="#ff0000" id="logout" value="Log Out"></a>
     </header>
 
     <div class="container">
 
       <!--Unfinished tasks-->
       <ul class="todo" id= "todo"></ul>
-
       <!-- Finished tasks-->
       <ul class="todo" id="completed"></ul>
     </div>
-
+  <body>
+  </body>
 
     <!--Javasript-->
     <script src = "resources\js\main.js"></script>
 
 </html>
+<?php
+}
+  else {
+    header("location:login.php");
+  }
+
+ ?>
--- todolist/app/login.php
+++ todolist/app/login.php
@@ -9,6 +9,8 @@
     $count = mysqli_num_rows($result);
     if($count > 0)
       {
+        session_start();
+        $_SESSION['loggedin'] = '1';
         header("location:list.php");
       }
     else
@@ -38,7 +40,7 @@
            <h1>Log In</h1>
            <input type="text" name="username" placeholder="Your username" required>
         <!--   <input type="email" name="email" placeholder="Email" required> -->
-           <input type="password" name="password" value="" required>
+        <input type="password" name="password" value="" id="password" placeholder="Password" required>
            <input type="checkbox" onclick="showpass()"> <h5>Show Password</h5>
            <input type="submit" id="login" name="login" value="Log In">
            <a href = "index.php"><input type = "button" value="Sign Up"></a>
